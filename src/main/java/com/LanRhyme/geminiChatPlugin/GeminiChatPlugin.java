67ec4d803e540999dffb431e66fef125a4fb00b1
LanRhyme <xiao_ren233@foxmail.com>
2025-02-05 21:42:23 +0800

    Initial commit

---
+++ src/main/java/com/LanRhyme/geminiChatPlugin/GeminiChatPlugin.java
@@ -0,0 +1,17 @@
+package com.LanRhyme.geminiChatPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GeminiChatPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/LanRhyme/geminiChatPlugin/PresetHandler.java
@@ -0,0 +1,2 @@
+package com.LanRhyme.geminiChatPlugin;public class PresetHandler {
+}
